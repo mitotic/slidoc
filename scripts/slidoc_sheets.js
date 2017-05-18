ee0ff6de1298df5e2eaecdae3e79e4ad02fd0d3c
R. Saravanan <sarava@sarava.net>
2017-05-17 20:57:32 -0500

    Prep for v0.97.5

--- scripts/slidoc_sheets.js
+++ scripts/slidoc_sheets.js
@@ -1,6 +1,6 @@
 // slidoc_sheets.js: Google Sheets add-on to interact with Slidoc documents
 
-var VERSION = '0.97.4q';
+var VERSION = '0.97.5';
 
 var DEFAULT_SETTINGS = [ ['auth_key', 'testkey', 'Secret value for secure administrative access (obtain from proxy for multi-site setup)'],
 
--- src/sdproxy.py
+++ src/sdproxy.py
@@ -45,7 +45,7 @@ from tornado.ioloop import IOLoop
 import reload
 import sliauth
 
-VERSION = '0.97.4q'
+VERSION = '0.97.5'
 
 UPDATE_PARTIAL_ROWS = True
 
